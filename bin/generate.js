c483636ff9fc04aa787512c48c87234e9805f780
Carlos Rodriguez <carlos@s8f.org>
2013-05-15 20:21:47 -0700

    fix cli version

--- bin/generate.js
+++ bin/generate.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 var argv = require('optimist')
-  .usage('Usage: $0 [-n] challenge')
+  .usage('Usage: $0 [-s <strength>] challenge')
   .demand(1)
   .alias('s', 'strength')
   .argv
-  , hashcash = require('../')(argv._[0], argv)
-  ;
+  , hashcash = require('../')(argv._[0], argv.strength)
 
 process.stdout.write(hashcash);
 
