e964538c99010242a6676e799af21f45ebb1bdc2
Carlos Rodriguez <carlos@s8f.org>
2013-05-17 10:55:04 -0700

    browser version doesn't need dish module packaged

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 REPORTER = spec
 
 all:
-	./node_modules/.bin/browserify -s hashcashgen index.js > browser/hashcashgen.js
+	./node_modules/.bin/browserify -s hashcashgen hashcashgen.js > browser/hashcashgen.js
 
 test:
 	@NODE_ENV=test ./node_modules/.bin/mocha --timeout 60000 --reporter $(REPORTER) --bail
--- index.js
+++ index.js
@@ -1,75 +1,7 @@
-var crypto = require('crypto')
-  , default_strength = 3
+var dish = require('dish');
 
-module.exports = exports = function generateHashCash (challenge, strength, cb) {
-  if (typeof strength === 'function') {
-    cb = strength;
-    strength = default_strength;
-  }
-  else if (!strength) {
-    strength = default_strength;
-  }
-  var search = repeat('0', strength)
-    , separator = ':'
-    , counter = 0
-    , attempt
-
-  function doAttempt () {
-    attempt = challenge + separator + counter++;
-    return exports.check(challenge, strength, attempt, search) ? attempt : null;
-  }
-
-  if (cb) {
-    (function findHashcash () {
-      var ret = doAttempt();
-      if (ret) return cb(null, ret);
-      else {
-        if (global.setImmediate) {
-          setImmediate(findHashcash);
-        }
-        else {
-          process.nextTick(findHashcash);
-        }
-      }
-    })();
-  }
-  else {
-    var ret;
-    do {
-      ret = doAttempt();
-    }
-    while (!ret);
-    return ret;
-  }
-};
-
-exports.check = function checkHashCash (challenge, strength, hashcash, search) {
-  if (typeof strength === 'string') {
-    hashcash = strength;
-    strength = default_strength;
-  }
-  else {
-    strength || (strength = default_strength);
-  }
-  if (!search) search = repeat('0', strength);
-  return (hashcash.indexOf(challenge) === 0 && sha1(hashcash).indexOf(search) === 0);
-};
+module.exports = exports = require('./hashcashgen');
 
 exports.middleware = function exportMiddleware (options) {
-  var dish = require('dish');
   return dish.file(__dirname + '/browser/hashcashgen.js', options);
 };
-
-function repeat (input, length) {
-  var ret = '';
-  for (var i = 0; i < length; i++) {
-    ret += input;
-  }
-  return ret;
-}
-
-function sha1 (input) {
-  return crypto.createHash('sha1')
-    .update(input)
-    .digest('hex');
-}
